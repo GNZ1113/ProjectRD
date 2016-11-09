e45e01c713ac22de481befa0a27e5b8799aba497
GNZ1113 <B.HUNTER1113@gmail.com>
2016-11-09 19:55:58 +0900

    Rune使用制限時間を追加

---
+++ STG/Assets/Scripts/Reload.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Hoge : MonoBehaviour
+{
+    private float reloadTimeOwn;
+    private float reloadTime;
+    private string reloadTimeStr;
+
+    //  Numがカスタムプロパティ
+
+    void Update()
+    {
+        reloadTime = reloadTimeOwn;
+        DG.Tweening.DOTween.To(() => reloadTime, (n) => reloadTime = n, 0, reloadTimeOwn);
+    }
+}
